04b9ac231e9335d60548695d865f7dc105ff502f
Tymoshenko Roman <tymoshenkorv@gmail.com>
2026-06-10 22:41:17 +0300

    gallery without mockup

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,12 @@
 console.log('Gallery');
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+console.log(SimpleLightbox);
+new SimpleLightbox('.gallery-link', {
+  overlay: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});

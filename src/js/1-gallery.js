6e4ecdfa7cef81b4454a877b76f0a11c6cce5cfb
Tymoshenko Roman <tymoshenkorv@gmail.com>
2026-06-11 11:17:08 +0300

    Task1 is done

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -7,47 +7,47 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const images = [
   {
     preview: '../img/thumbs/1.jpg',
-    original: '../img/thumbs/1.jpg',
+    original: '../img/originals/1.jpg',
     description: 'Image 1',
   },
   {
     preview: '../img/thumbs/2.jpg',
-    original: '../img/thumbs/2.jpg',
+    original: '../img/originals/2.jpg',
     description: 'Image 2',
   },
   {
     preview: '../img/thumbs/3.jpg',
-    original: '../img/thumbs/3.jpg',
+    original: '../img/originals/3.jpg',
     description: 'Image 3',
   },
   {
     preview: '../img/thumbs/4.jpg',
-    original: '../img/thumbs/4.jpg',
+    original: '../img/originals/4.jpg',
     description: 'Image 4',
   },
   {
     preview: '../img/thumbs/5.jpg',
-    original: '../img/thumbs/5.jpg',
+    original: '../img/originals/5.jpg',
     description: 'Image 5',
   },
   {
     preview: '../img/thumbs/6.jpg',
-    original: '../img/thumbs/6.jpg',
+    original: '../img/originals/6.jpg',
     description: 'Image 6',
   },
   {
     preview: '../img/thumbs/7.jpg',
-    original: '../img/thumbs/7.jpg',
+    original: '../img/originals/7.jpg',
     description: 'Image 7',
   },
   {
     preview: '../img/thumbs/8.jpg',
-    original: '../img/thumbs/8.jpg',
+    original: '../img/originals/8.jpg',
     description: 'Image 8',
   },
   {
     preview: '../img/thumbs/9.jpg',
-    original: '../img/thumbs/9.jpg',
+    original: '../img/originals/9.jpg',
     description: 'Image 9',
   },
 ];
